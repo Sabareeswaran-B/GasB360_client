e587d7b992c927e3041cc0ef7c34e2dfe960e23c
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-03-31 10:43:39 +0530

    customer login and signup component added

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomerLoginComponent } from './customer-login/customer-login.component';
+import { CustomerSignupComponent } from './customer-signup/customer-signup.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CustomerLoginComponent,
+    CustomerSignupComponent
+  ],
   imports: [
     CommonModule
   ]
