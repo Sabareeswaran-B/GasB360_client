8adaa01c53fa094bbee6ef40c98bbb9254c51436
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-04-09 19:27:41 +0530

    base url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: "https://gasb360api.azurewebsites.net"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: "https://localhost:7076"
+  // baseUrl: "https://localhost:7076"
+  baseUrl: "https://gasb360api.azurewebsites.net"
 };
 
 /*
