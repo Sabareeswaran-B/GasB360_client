66699ab2d06e818312c6758ef0132becf11ef8a6
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-04-10 12:49:38 +0530

    admin and delivery guard added

--- src/app/delivery/delivery-router/delivery-router.module.ts
+++ src/app/delivery/delivery-router/delivery-router.module.ts
@@ -4,16 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { DeliveryCompletedordersComponent } from '../delivery-completedorders/delivery-completedorders.component';
 import { DeliveryOrdersComponent } from '../delivery-orders/delivery-orders.component';
 import { DeliveryDashboardComponent } from '../delivery-dashboard/delivery-dashboard.component';
+import { AuthGuard } from 'src/app/guard/auth.guard';
+import { DeliveryGuard } from 'src/app/guard/delivery.guard';
 
 const routes: Routes = [
-  {path:"delivery/ordersbyemployee",component:DeliveryOrdersComponent},
-  {path:"delivery/deliveredorders",component:DeliveryCompletedordersComponent},
-  {path:"delivery/dashboard",component:DeliveryDashboardComponent}
+  { path: "delivery/ordersbyemployee", component: DeliveryOrdersComponent, canActivate: [AuthGuard, DeliveryGuard] },
+  { path: "delivery/deliveredorders", component: DeliveryCompletedordersComponent, canActivate: [AuthGuard, DeliveryGuard] },
+  { path: "delivery/dashboard", component: DeliveryDashboardComponent, canActivate: [AuthGuard, DeliveryGuard] }
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forChild(routes),CommonModule],
+  imports: [RouterModule.forChild(routes), CommonModule],
   exports: [RouterModule]
 })
 export class DeliveryRouterModule { }
